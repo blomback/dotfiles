d72fab6ae4a440909fcf1b82401858315a4b3623
Andreas Blombäck <andreas@blomback.se>
2015-09-12 14:21:11 +0200

    Added sublime builds and snippets

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # get current dir
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export SUBLIME_DIR="$DOTFILES_DIR/sublime"
 
 # update dotfiles from repo
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
@@ -17,7 +18,10 @@ ln -sfv "$DOTFILES_DIR/com/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 
 # sublime settings
-ln -sfv "$DOTFILES_DIR/sublime/AdvancedNewFile.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/AdvancedNewFile.sublime-settings
-ln -sfv "$DOTFILES_DIR/sublime/Base File.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Base\ File.sublime-settings
-ln -sfv "$DOTFILES_DIR/sublime/Package Control.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
-ln -sfv "$DOTFILES_DIR/sublime/Preferences.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -sfv "$SUBLIME_DIR/AdvancedNewFile.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/AdvancedNewFile.sublime-settings
+ln -sfv "$SUBLIME_DIR/Base File.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Base\ File.sublime-settings
+ln -sfv "$SUBLIME_DIR/Package Control.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+ln -sfv "$SUBLIME_DIR/Preferences.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -sfv "$SUBLIME_DIR/Default (OSX).sublime-keymap" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
+ln -sfv "$SUBLIME_DIR/builds/" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/builds
+ln -sfv "$SUBLIME_DIR/snippets/" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets
