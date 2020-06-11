b0db4f254fe9d6bc3933ee58c5e73da0e2086d5c
Andreas Blombäck <andreas@blomback.se>
2020-06-11 14:50:11 +0200

    separated global and macos specific files in install.sh

--- install.sh
+++ install.sh
@@ -1,51 +1,38 @@
 #!/bin/sh
 
 # get current dir
-export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export SUBLIME_DIR=$DOTFILES_DIR/sublime
+export dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # update dotfiles from repo
-[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+[ -d "$dotfiles_dir/.git" ] && git --work-tree="$dotfiles_dir" --git-dir="$dotfiles_dir/.git" pull origin master
 
 # download base16 themes
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
-# run setup files
-for DOTFILE in "$DOTFILES_DIR"/{alias,macos}; do
-    [ -f "$DOTFILE" ] && source "$DOTFILE"
+# run global setup files
+for file in "$dotfiles_dir"/{.aliases}; do
+    [ -r "$file" ] && [ -f "$file" ] && source "$file"
 done
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-
-
+# macos specific settings
+if [ "$(uname)" == "Darwin" ]; then
+  for file in "$dotfiles_dir"/{.macos,.sublime,.nvm}; do
+    [ -r "$file" ] && [ -f "$file" ] && source "$file";
+  done;
+  unset file;
+fi;
 
 # symlinks
-ln -sfv "$DOTFILES_DIR/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/.zshrc" ~
-ln -sfv "$DOTFILES_DIR/.vimrc" ~
-ln -sfv "$DOTFILES_DIR/.gvimrc" ~
-ln -sfv "$DOTFILES_DIR/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/.hushlogin" ~
+ln -sfv "$dotfiles_dir/.bash_profile" ~
+ln -sfv "$dotfiles_dir/.zshrc" ~
+ln -sfv "$dotfiles_dir/.vimrc" ~
+ln -sfv "$dotfiles_dir/.gvimrc" ~
+ln -sfv "$dotfiles_dir/.gitconfig" ~
+ln -sfv "$dotfiles_dir/.hushlogin" ~
 
 # oh my zsh themes
-ln -sfv "$DOTFILES_DIR/oh-my-zsh/themes/gitster.zsh-theme" ~/.oh-my-zsh/themes/gitster.zsh-theme
+ln -sfv "$dotfiles_dir/oh-my-zsh/themes/gitster.zsh-theme" ~/.oh-my-zsh/themes/gitster.zsh-theme
 
-# sublime settings
-ln -sfv "$SUBLIME_DIR/AdvancedNewFile.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/AdvancedNewFile.sublime-settings
-ln -sfv "$SUBLIME_DIR/Base File.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Base\ File.sublime-settings
-ln -sfv "$SUBLIME_DIR/Package Control.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
-ln -sfv "$SUBLIME_DIR/Preferences.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-ln -sfv "$SUBLIME_DIR/Default (OSX).sublime-keymap" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
 
-if [ ! -d ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/builds ]; then
-	ln -sfv "$SUBLIME_DIR/builds/" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/builds > /dev/null 2>&1;
-fi
-
-if [ ! -d ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets ]; then
-	ln -sfv "$SUBLIME_DIR/snippets/" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets > /dev/null 2>&1;
-fi
-
-unset SUBLIME_DIR
-unset DOTFILES_DIR
-unset NVM_DIR
+unset dotfiles_dir;
+unset nvm_dir;
